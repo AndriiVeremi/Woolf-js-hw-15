74b1678cbda362b2fbb0553040c2914af346ddcb
Andrii Veremii <dashuk10@gmail.com>
2023-10-13 22:12:12 +0200

    add

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  button: document.querySelector(".change-color"),
+  colorName: document.querySelector(".color"),
+  body: document.querySelector('body')
+};
+
+console.log(refs.body)
+
+refs.button.addEventListener("click", onChangeColor);
+
+function onChangeColor(e) {
+  const newColor = getRandomHexColor();
+  refs.colorName.textContent = newColor;
+  refs.body.style.backgroundColor = `${newColor}`;
+}
