b46b264b670413ac32d03f31981e6cdb7a23cba7
Andrii Veremii <dashuk10@gmail.com>
2023-10-13 21:51:19 +0200

    add

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const { email, password } = e.currentTarget.elements;
+
+  if (email.value.length === 0 || password.value.length === 0) {
+    alert("Всі поля повинні бути заповнені !!!");
+  }
+
+  const user = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(user);
+
+  form.reset();
+}
