58b69398ffcf33de22d7de87684314c995a3f661
Andrii Veremii <dashuk10@gmail.com>
2023-10-14 17:20:29 +0200

    add

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,48 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  create: document.querySelector("button[data-create]"),
+  destroy: document.querySelector("button[data-destroy]"),
+  input: document.querySelector("input"),
+  collection: document.querySelector("#boxes"),
+};
+
+refs.input.addEventListener("input", onInputNumber);
+
+function onInputNumber() {
+  const amount = Number(refs.input.value);
+  return amount;
+}
+
+refs.create.addEventListener("click", () => {
+  createBoxes(Number(refs.input.value));
+});
+
+function createBoxes(amount) {
+  let startWidth = 30;
+  let startHeight = 30;
+  let arrDivs = [];
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${startWidth}px`;
+    div.style.height = `${startHeight}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    startWidth += 10;
+    startHeight += 10;
+
+    arrDivs.push(div);
+  }
+
+  refs.collection.append(...arrDivs);
+}
+
+refs.destroy.addEventListener("click", destroyBoxes);
+
+function destroyBoxes() {
+  refs.collection.innerHTML = "";
+  refs.input.value = "";
+}
